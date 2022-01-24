099c2ed9b4c68f51f6734ab7f44b23f2a84cd74d
akhileshmanish13 <akhileshmanish13@gmail.com>
2022-01-24 15:57:19 +0530

    CompareFuntion

---
+++ src/com/company/Compare_Function.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.*;
+
+public class Compare_Function {
+    // Compare Function
+    public static void compareNumber(int a, int b){
+        if( a > b ){
+            System.out.println(a + " is greater than " + b );
+        }
+        else{
+            System.out.println(b + " is greater than " + a);
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of a: ");
+        int a = sc.nextInt();
+        System.out.print("Enter the value of b: ");
+        int b = sc.nextInt();
+        compareNumber(a,b); // Function call
+    }
+}

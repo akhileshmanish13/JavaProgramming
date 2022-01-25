c36aec6ac3c7561b5cf15bc249a37bbe8b66d5ea
akhileshmanish13 <akhileshmanish13@gmail.com>
2022-01-25 13:53:49 +0530

    Changes in code

--- src/com/company/ArrayIntroduction.java
+++ src/com/company/ArrayIntroduction.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class ArrayIntroduction {
     public static void main(String[] args){
+        // Array of Numbers
         //Syntax of declaring array
         // 1. (data type) (arrayName)[] = (new) (datatype) [size]
         // 2. (datatype) (arrayName)[] = {a, b, c.......,n}
@@ -15,8 +16,22 @@ public class ArrayIntroduction {
         for(int i = 0; i< size; i++){
             marks[i] = sc.nextInt();
         }
+        System.out.print("marks [] = {");
         for (int i = 0; i < marks.length; i++) {
-            System.out.println("marks ["+ i +"] = "+marks[i] + " ");
+            System.out.print(+marks[i] + ",");
         }
+        System.out.println("}");
+    // ============================================================================================================
+        // Array of Strings
+        String [] name = new String[size];
+        System.out.println("Enter the names of Students :");
+        for (int i = 0; i < size; i++ ){
+            name[i] = sc.next();
+        }
+        System.out.print("name [] = { ");
+        for(int i =0 ; i < name.length; i++){
+            System.out.print( name[i] +", " );
+        }
+        System.out.print("}");
     }
 }
